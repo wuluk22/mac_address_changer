11a1bf43bde5d20e92dfec3e9d0d955de5bfed77
reb <reb@FURBIFY-BDBSHKB.home>
2026-01-29 09:27:36 +0100

    refactor + validation

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -20,5 +21,21 @@ def change_mac(interface, mac_address):
     subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] No MAC address found")
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("[+] Current MAC address: " + str(current_mac))
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to: " + str(current_mac))
+else:
+    print("[-] MAC address was not changed to: " + str(options.new_mac))
